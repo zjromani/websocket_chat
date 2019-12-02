7b296d0c8c0d74761701e6a27fc4964ecfdf8cdf
Zach Romani <zjromani@gmail.com>
2019-12-01 22:10:39 -0700

    Add current user
    Fake the current user functionality by setting a random User's name in
    the cookies.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user ||= if cookies['name']
+      User.find_or_create_by(name: cookies['name'])
+    else
+      user = User.all.sample
+      cookies['name'] = user.name
+      user
+    end
+  end
 end
