ee4e02617529cf08ba2367ac3c4e2c38b7f8176e
Zach Romani <zjromani@gmail.com>
2019-12-02 20:34:59 -0700

    Add current user for websocket
    Using the same hacky  session managment with cookies as I did in
    ApplicationController just to speed dev time up

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = set_current_user
+      logger.add_tags 'ActionCable', "User #{current_user.name}"
+    end
+
+    protected
+
+    def set_current_user
+      if cookies['name']
+        User.find_by(name: cookies['name'])
+      else
+        user = User.all.sample
+        cookies['name'] = user.name
+        user
+      end
+    end
   end
 end
